cc5a294eeb4c04af2dce69570857df21553d7fb6
KiarashYavari <kiarash996@gmail.com>
2022-12-05 12:12:01 +0000

    fix post model indentation

--- blog/models.py
+++ blog/models.py
@@ -19,17 +19,16 @@ class Comment(models.Model):
 
 
 class Post(models.Model):
-	author = models.ForeignKey(settings.AUTH_USER_MODEL,
-	on_delete=models.PROTECT)
-	created_at = models.DateTimeField(auto_now_add=True)
-	modified_at = models.DateTimeField(auto_now=True)
-	published_at = models.DateTimeField(blank=True, null=True)
-	title = models.TextField(max_length=100)
-	slug = models.SlugField()
-	summary = models.TextField(max_length=500)
-	content = models.TextField()
-	tags = models.ManyToManyField(Tag, related_name="posts")
+  author = models.ForeignKey(settings.AUTH_USER_MODEL,
+  on_delete=models.PROTECT)
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
+  published_at = models.DateTimeField(blank=True, null=True)
+  title = models.TextField(max_length=100)
+  slug = models.SlugField()
+  summary = models.TextField(max_length=500)
+  content = models.TextField()
+  tags = models.ManyToManyField(Tag, related_name="posts")
   comments = GenericRelation(Comment)
-
-	def __str__(self):
-		return self.title
+  def __str__(self):
+    return self.title
