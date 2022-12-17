dc2f1890cd5b6471aab540e072c651391a773047
KiarashYavari <kiarash996@gmail.com>
2022-12-17 16:07:41 +0330

    rebuild form with one crispy template tag

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from blog.models import Comment
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class CommentForm(forms.ModelForm):
+  def __init__(self, *args, **kwargs):
+    super(CommentForm, self).__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('send', 'Send'))
+  
+  
   class Meta:
     model = Comment
     fields = ["content"]
+    
+    
