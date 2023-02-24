75dc77f397650cf847d2d6ef3e9de72a3a229ac8
KiarashYavari <kiarash996@gmail.com>
2023-02-24 12:22:16 +0330

    recreate db and loaddata from data.json to db

--- blango_auth/admin.py
+++ blango_auth/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from blango_auth.models import User
-from django.contrib.auth.admin import AdminUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
