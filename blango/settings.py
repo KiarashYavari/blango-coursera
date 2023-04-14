847134bc0ca1b67fdb140ae941e2610830ae1cf3
Kiarash Yavari <kiarash996@gmail.com>
2023-04-14 09:42:39 +0000

    add login with google

--- blango/settings.py
+++ blango/settings.py
@@ -40,8 +40,13 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blog',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
@@ -79,8 +84,15 @@ class Dev(Configuration):
 
   WSGI_APPLICATION = 'blango.wsgi.application'
 
+  SITE_ID = 1
+
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
   ACCOUNT_ACTIVATION_DAYS = 7
+
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
 
   # Database
   # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
